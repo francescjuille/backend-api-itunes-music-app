c478d96ebe9e327b27e476a52f28fb6764b4d316
Francesc <francescjuille@gmail.com>
2022-01-19 12:00:22 +0100

    fix: minor fix

--- config/default.js
+++ config/default.js
@@ -3,13 +3,6 @@ module.exports = {
         port: '4000',
         domain: 'localhost'
     },
-    mysql: {
-        port: 3306,
-        user: "root",
-        password: 'root',
-        database: "user",
-        host: "127.0.0.1",
-        dialect: "mysql"
-    },
+    
     logger: 'dev',
 }
